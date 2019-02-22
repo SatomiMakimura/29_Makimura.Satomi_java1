420e9f2e178eab0b050a5d918c981a136d583626
SatomiMakimura <s.an7.atom.1@gmail.com>
2019-02-22 23:15:39 +0900

    ver.2

--- src/shindan_kadai.java
+++ src/shindan_kadai.java
@@ -19,84 +19,90 @@ public class shindan_kadai {
         Answer[3] = "1:買い物をする　2:カラオケに行く　3:運動をする　4:お酒を飲む　5:その他";
         Answer[4] = "1:小説　2:ノンフィクション　3:自己啓発　4:趣味系　5:参考書";
 
+        int score;
+        int num0 = 0;
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
 
 
-        for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 5; i++) {
 
-            System.out.println(question[i]);
-            System.out.println(Answer[i]);
+                System.out.println(question[i]);
+                System.out.println(Answer[i]);
 
-            int kaitou = new java.util.Scanner(System.in).nextInt();
+                int kaitou = new java.util.Scanner(System.in).nextInt();
 
 
-//        Answer[0] = "1:家でのんびり 2:遠くまでドライブ 3:話題のスポットにいく 4:友達と会う";
-            if (i == 0) {
-                if ( kaitou == 1) {
-                    int num0 = 0;
-                } else if (kaitou == 2) {
-                    int num0 =5;
-                } else if (kaitou == 3) {
-                    int num0 =0;
-                } else if (kaitou == 4) {
-                    int num0 =10;
+    //        Answer[0] = "1:家でのんびり 2:遠くまでドライブ 3:話題のスポットにいく 4:友達と会う";
+                if (i == 0) {
+                    if (kaitou == 1) {
+                        num0 = 0;
+                    } else if (kaitou == 2) {
+                        num0 = 5;
+                    } else if (kaitou == 3) {
+                        num0 = 0;
+                    } else if (kaitou == 4) {
+                        num0 = 10;
+                    }
+                }
+    //        Answer[1] = "1:とても好き　2:まぁまぁ好き　3:あまり好きじゃない　4;むしろきらい";
+                if (i == 1) {
+                    if (kaitou == 1) {
+                        num1 = 5;
+                    } else if (kaitou == 2) {
+                        num1 = 10;
+                    } else if (kaitou == 3) {
+                        num1 = 5;
+                    } else if (kaitou == 4) {
+                        num1 = 0;
+                    }
+                }
+    //        Answer[2] = "1:家で飲む　2:友達と飲みに行く　3:仕事の付き合い程度　4:全く飲まない";
+                if (i == 2) {
+                    if (kaitou == 1) {
+                        num2 = 0;
+                    } else if (kaitou == 2) {
+                        num2 = 10;
+                    } else if (kaitou == 3) {
+                        num2 = 5;
+                    } else if (kaitou == 4) {
+                        num2 = 0;
+                    }
+                }
+    //        Answer[3] = "1:買い物をする　2:カラオケに行く　3:運動をする　4:お酒を飲む　5:その他";
+                if (i == 3) {
+                    if (kaitou == 1) {
+                        num3 = 10;
+                    } else if (kaitou == 2) {
+                        num3 = 5;
+                    } else if (kaitou == 3) {
+                        num3 = 5;
+                    } else if (kaitou == 4) {
+                        num3 = 0;
+                    } else if (kaitou == 5) {
+                        num3 = 0;
+                    }
+                }
+
+    //        Answer[4] = "1:小説　2:ノンフィクション　3:自己啓発　4:趣味系　5:参考書";
+                if (i == 4) {
+                    if (kaitou == 1) {
+                        num4 = 10;
+                    } else if (kaitou == 2) {
+                        num4 = 0;
+                    } else if (kaitou == 3) {
+                        num4 = 5;
+                    } else if (kaitou == 4) {
+                        num4 = 5;
+                    } else if (kaitou == 5) {
+                        num4 = 0;
+                    }
                 }
             }
-//        Answer[1] = "1:とても好き　2:まぁまぁ好き　3:あまり好きじゃない　4;むしろきらい";
-            if (i == 1) {
-                if ( kaitou == 1) {
-                    int num1 = 5;
-                } else if (kaitou == 2) {
-                    int num1 = 10;
-                } else if (kaitou == 3) {
-                    int num1 = 5;
-                } else if (kaitou == 4) {
-                    int num1 = 0;
-                }
-            }
-//        Answer[2] = "1:家で飲む　2:友達と飲みに行く　3:仕事の付き合い程度　4:全く飲まない";
-            if (i == 2) {
-                if ( kaitou == 1) {
-                    int num2 = 0;
-                } else if (kaitou == 2) {
-                    int num2 = 10;
-                } else if (kaitou == 3) {
-                    int num2 = 5;
-                } else if (kaitou == 4) {
-                    int num2 = 0;
-                }
-            }
-//        Answer[3] = "1:買い物をする　2:カラオケに行く　3:運動をする　4:お酒を飲む　5:その他";
-            if (i == 3) {
-                if ( kaitou == 1) {
-                    int num3 = 10;
-                } else if (kaitou == 2) {
-                    int num3 = 5;
-                } else if (kaitou == 3) {
-                    int num3 = 5;
-                } else if (kaitou == 4) {
-                    int num3 = 0;
-                } else if (kaitou == 5) {
-                    int num3 = 0;
-                }
-            }
 
-//        Answer[4] = "1:小説　2:ノンフィクション　3:自己啓発　4:趣味系　5:参考書";
-            if (i == 4) {
-                if ( kaitou == 1) {
-                    int num4 = 10;
-                } else if (kaitou == 2) {
-                    int num4 = 0;
-                } else if (kaitou == 3) {
-                    int num4 = 5;
-                } else if (kaitou == 4) {
-                    int num4 = 5;
-                } else if (kaitou == 5) {
-                    int num4 = 0;
-                }
-            }
-
-            int score;
-            score = num0 + num1 + num2 + num3 + num4;
+        score = num0 + num1 + num2 + num3 + num4;
 
 
             if (score >= 40) {
@@ -114,5 +120,5 @@ public class shindan_kadai {
         }
     }
 
-}
+
 
